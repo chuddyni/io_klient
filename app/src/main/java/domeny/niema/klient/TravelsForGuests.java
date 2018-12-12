50670fc879c564ffa0ff4a859763705ab62deb72
Vethavear <adrian.bury@gmail.com>
2018-12-12 19:33:41 +0100

    Release 1.0

--- app/src/main/java/domeny/niema/klient/TravelsForGuests.java
+++ app/src/main/java/domeny/niema/klient/TravelsForGuests.java
@@ -1,11 +1,14 @@
 package domeny.niema.klient;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class TravelsForGuests extends AppCompatActivity {
 
@@ -24,5 +27,24 @@ public class TravelsForGuests extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        final Button back_button = findViewById(R.id.back_button);
+        back_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ProgressDialog dlg = new ProgressDialog(TravelsForGuests.this);
+                dlg.setTitle("Please, wait a moment.");
+                dlg.setMessage("Returning to the login section...");
+                dlg.show();
+                Intent intent = new Intent(TravelsForGuests.this, LoginActivity.class);
+                dlg.dismiss();
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
